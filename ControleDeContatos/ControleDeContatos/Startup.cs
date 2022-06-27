96ef5e5e07ec67d7c2757c8fb1dfa336c5c603ac
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-27 13:17:22 -0300

    SMTP redefinicao de senha

--- ControleDeContatos/ControleDeContatos/Startup.cs
+++ ControleDeContatos/ControleDeContatos/Startup.cs
@@ -32,6 +32,7 @@ namespace ControleDeContatos
             services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
             services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
             services.AddScoped<ISessao, Sessao>();
+            services.AddScoped<IEmail, Email>();
 
             services.AddSession(o => {
                 o.Cookie.HttpOnly = true;
