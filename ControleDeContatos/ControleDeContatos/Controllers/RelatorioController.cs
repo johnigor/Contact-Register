a596956d316964c94c492f19f579dfdcaa7f8a07
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-28 16:28:05 -0300

    Armazenando registro de busca na navbar dos relatorios

--- ControleDeContatos/ControleDeContatos/Controllers/RelatorioController.cs
+++ ControleDeContatos/ControleDeContatos/Controllers/RelatorioController.cs
@@ -21,6 +21,12 @@ namespace ControleDeContatos.Controllers
         }
         public IActionResult BuscarPorData(DateTime? minDate, DateTime? maxDate)
         {
+            if (!minDate.HasValue) minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            if (!maxDate.HasValue) maxDate = DateTime.Now;
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
             var result = _relatorioService.FindByDate(minDate, maxDate);
             return View(result);
         }
